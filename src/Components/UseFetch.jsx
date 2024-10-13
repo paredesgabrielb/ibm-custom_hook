7b42c299394755f1faafcc6065510357d68bee15
Gabriel Paredes <paredesgabrielb@gmail.com>
2024-10-13 18:09:13 -0400

    implement UseFetch

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,15 @@
+import {useState, useFetch} from 'react';
+
 const UseFetch = (url) => {
-    
+    const [data, setData] = useState([]);
+    useFetch(()=>{
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {setData(data)} )
+            .catch((e) => { console.error(e)})
+    },[]);
+
+    return [data];
 }
 
 export default UseFetch
